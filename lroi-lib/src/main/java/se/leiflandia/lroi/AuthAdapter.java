355919b39b5eb37f8597f9ba974060cae773b7f0
Kristofer Leifland <kristofer.leifland@afconsult.com>
2015-03-29 13:01:35 +0200

    Bugfix.

--- lroi-lib/src/main/java/se/leiflandia/lroi/AuthAdapter.java
+++ lroi-lib/src/main/java/se/leiflandia/lroi/AuthAdapter.java
@@ -89,6 +89,7 @@ public class AuthAdapter {
                         } else {
                             kind = SignupFailure.UNEXPECTED;
                         }
+                        break;
                     default:
                         kind = SignupFailure.UNEXPECTED;
                 }
