abd4c98817c94f107cdea2038e572a5a515fb2fc
Kristofer Leifland <kristofer.leifland@afconsult.com>
2015-05-05 12:48:31 +0200

    Bugfix, fixed sample app.

--- sample/src/main/java/se/leiflandia/sample/lroi/MainActivity.java
+++ sample/src/main/java/se/leiflandia/sample/lroi/MainActivity.java
@@ -1,11 +1,8 @@
 package se.leiflandia.sample.lroi;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,7 +10,7 @@ import se.leiflandia.lroi.AuthAdapter;
 import se.leiflandia.lroi.network.SignoutFailure;
 import se.leiflandia.lroi.utils.Callback;
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     private AuthAdapter auth;
 
