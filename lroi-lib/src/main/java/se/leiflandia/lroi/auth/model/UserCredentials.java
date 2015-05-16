c1ef63b087b9a8542466d65adf86e18338410055
Kristofer Leifland <kristofer.leifland@gmail.com>
2015-05-16 14:41:46 +0200

    Added builder for UserCredentials.

--- lroi-lib/src/main/java/se/leiflandia/lroi/auth/model/UserCredentials.java
+++ lroi-lib/src/main/java/se/leiflandia/lroi/auth/model/UserCredentials.java
@@ -1,5 +1,7 @@
 package se.leiflandia.lroi.auth.model;
 
+import android.os.Build;
+
 public class UserCredentials extends ClientCredentials {
 
     private String username;
@@ -23,4 +25,45 @@ public class UserCredentials extends ClientCredentials {
         return grant_type;
     }
 
+    public static class Builder {
+        private final ClientCredentials clientCredentials;
+        private String username;
+        private String password;
+        private String grant_type;
+
+        private Builder(ClientCredentials clientCredentials) {
+            this.clientCredentials = clientCredentials;
+        }
+
+        public static Builder with(ClientCredentials clientCredentials) {
+            return new Builder(clientCredentials);
+        }
+
+        public static Builder with(String clientId, String clientSecret) {
+            return new Builder(new ClientCredentials(clientId, clientSecret));
+        }
+
+        public Builder setUsername(String username) {
+            this.username = username;
+            return this;
+        }
+
+        public Builder setPassword(String password) {
+            this.password = password;
+            return this;
+        }
+
+        public Builder setGrantType(String grant_type) {
+            this.grant_type = grant_type;
+            return this;
+        }
+
+        public UserCredentials build() {
+            return new UserCredentials(
+                    clientCredentials,
+                    username,
+                    password,
+                    grant_type);
+        }
+    }
 }
